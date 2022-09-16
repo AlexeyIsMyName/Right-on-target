d931f7ed7e2b180d0d0bb17ef801c4a31cecbad5
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-09-17 00:32:10 +0300

    make Game Model as a class

--- Right on target/Model/Game.swift
+++ Right on target/Model/Game.swift
@@ -26,3 +26,61 @@ protocol GameProtocol {
     // Сравнивает переданное значение с загаданным и начисляет очки
     func calculateScore(with value: Int)
 }
+
+class Game: GameProtocol {
+    var score: Int = 0
+    
+    private var minSecretValue: Int // Минимальное загаданное значение
+    private var maxSecretValue: Int // Максимальное загаданное значение
+    
+    var currentSecretValue: Int = 0
+    
+    private var lastRound: Int // Количество раундов
+    private var currentRound: Int = 1
+    
+    var isGameEnded: Bool {
+        if currentRound >= lastRound { return true
+        } else {
+            return false
+        }
+    }
+    
+    init?(startValue: Int, endValue: Int, rounds: Int) {
+        // Стартовое значение для выбора случайного числа не может быть больше конечного
+        guard startValue <= endValue else {
+            return nil
+        }
+        
+        minSecretValue = startValue
+        maxSecretValue = endValue
+        lastRound = rounds
+        currentSecretValue = self.getNewSecretValue()
+    }
+    
+    func restartGame() {
+        currentRound = 0
+        score = 0
+        startNewRound()
+    }
+    
+    func startNewRound() {
+        currentSecretValue = self.getNewSecretValue()
+        currentRound += 1
+    }
+    
+    // Загадать и вернуть новое случайное значение
+    private func getNewSecretValue() -> Int {
+        (minSecretValue...maxSecretValue).randomElement()!
+    }
+    
+    // Подсчитывает количество очков
+    func calculateScore(with value: Int) {
+        if value > currentSecretValue {
+            score += 50 - value + currentSecretValue
+        } else if value < currentSecretValue {
+            score += 50 - currentSecretValue + value
+        } else {
+            score += 50
+        }
+    }
+}
